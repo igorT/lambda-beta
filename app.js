e6006d36f393524b69cbe2e436a80e838d0417c5
Igor Terzic <iterzic@linkedin.com>
2019-04-24 07:32:27 -0700

    Beta clean works

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var ApiBuilder = require('claudia-api-builder'),
 
 module.exports = api;
 
-api.get('/hello', function () {
+api.post('/pushed', function (request) {
     let ssh = process.env.ssh;
+
     execSync('rm -rf /tmp/*', { encoding: 'utf8', stdio: 'inherit' })
     fs.writeFileSync('/tmp/known_hosts', 'github.com,192.30.252.*,192.30.253.*,192.30.254.*,192.30.255.* ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==')
 
@@ -22,11 +23,17 @@ ${ssh.split(' ').join('\n')}
 
     process.env.GIT_SSH_COMMAND = 'ssh -o UserKnownHostsFile=/tmp/known_hosts -i /tmp/id_rsa'
   
-    execSync('cat /tmp/id_rsa', { encoding: 'utf8', stdio: 'inherit' })
+    execSync('git clone ssh://git@github.com/igorT/data-orbit.git /tmp/data-orbit', { encoding: 'utf8', stdio: 'inherit' })
+    execSync('cd /tmp/data-orbit &&   git config user.email "lambda+terzicigor@gmail.com"', { encoding: 'utf8', stdio: 'inherit' })
+    execSync('cd /tmp/data-orbit && git config user.name "lambda beta bot"', { encoding: 'utf8', stdio: 'inherit' })
+    //execSync(`cd /tmp/data-orbit && git remote update && git fetch origin`, { encoding: 'utf8', stdio: 'inherit' })
 
-    execSync('git clone --depth 1 ssh://git@github.com/igorT/data-orbit.git /tmp/data-orbit', { encoding: 'utf8', stdio: 'inherit' })
-
-    execSync('cd /tmp/data-orbit && git checkout -b lambda && git push --set-upstream origin lambda', { encoding: 'utf8', stdio: 'inherit' })
   
-    return execSync('ls /tmp/aws4', { encoding: 'utf8' }).split('\n')
+   if (request.body.ref === 'refs/heads/master') {
+       request.body.commits.forEach(commit => {
+        //execSync(`cat ${JSON.stringify(commit)}`);
+        execSync(`cd /tmp/data-orbit && git checkout beta && git cherry-pick ${commit.id}   && git push origin beta`, { encoding: 'utf8', stdio: 'inherit' })
+       });
+   }
+   return request.body;
 });
